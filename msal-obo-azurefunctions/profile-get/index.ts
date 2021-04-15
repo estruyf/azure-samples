885dfd891928292691c39181c37dac2e3ad52ae5
Elio Struyf <elio@struyfconsulting.be>
2021-04-15 12:30:17 +0200

    Added Azure Function with OBO flow

---
+++ msal-obo-azurefunctions/profile-get/index.ts
@@ -0,0 +1,106 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import jwt, { JwtHeader, SigningKeyCallback } from "jsonwebtoken";
+import jwksClient from "jwks-rsa";
+import { ConfidentialClientApplication, OnBehalfOfRequest } from "@azure/msal-node";
+import fetch from "node-fetch";
+
+// For simplicity the config has been added to the function
+// When using this in production, make sure to move these keys to Azure KeyVault
+const config = {
+  auth: {
+    clientId: "<client-id>",
+    clientSecret: "<client-secret>",
+    authority: "https://login.microsoftonline.com/common"
+  }
+};
+
+/**
+ * Retrieve the signing key
+ * @param header 
+ * @returns 
+ */
+const getSigningKeys = (header: JwtHeader, callback: SigningKeyCallback) => {
+  const client = jwksClient({
+    jwksUri: 'https://login.microsoftonline.com/common/discovery/keys'
+  });
+  
+  client.getSigningKey(header.kid, function (err, key: any) {
+    callback(null, key.publicKey || key.rsaPublicKey);
+  });
+}
+
+/**
+ * Validate the JWT Token
+ * @param req 
+ */
+const validateToken = (req: HttpRequest): Promise<string> => {
+  return new Promise(async (resolve, reject) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.split(' ').pop();
+      
+      const validationOptions = {
+        audience: `api://${config.auth.clientId}`
+      }
+
+      jwt.verify(token, getSigningKeys, validationOptions, (err, payload) => {
+        if (err) {
+          reject(403);
+          return;
+        }
+        
+        resolve(token);
+      });
+    } else {
+      reject(401);
+    }
+  });
+};
+
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+  try {
+    const token = await validateToken(req);
+
+    const oboRequest: OnBehalfOfRequest = {
+      oboAssertion: token,
+      scopes: ["user.read"],
+    }
+
+    try {
+      const cca = new ConfidentialClientApplication(config);
+      const response = await cca.acquireTokenOnBehalfOf(oboRequest);
+
+      if (response && response.accessToken) {
+        const graphResponse = await fetch(`https://graph.microsoft.com/v1.0/me`, {
+          headers: {
+            "authorization": `Bearer ${response.accessToken}`
+          }
+        });
+
+        if (graphResponse && graphResponse.ok) {
+          const data = await graphResponse.json();
+          
+          context.res = {
+            body: data,
+            headers: {
+              "content-type": "application/json"
+            }
+          };
+          return;
+        } else {
+          throw graphResponse.status;
+        }
+      } else {
+        throw 403;
+      }
+    } catch (e) {
+      context.log(`ERROR: ${e.message}`);
+      throw 500;
+    }
+  } catch(e) {
+    context.res.status(e);
+  }
+};
+
+export default httpTrigger;
